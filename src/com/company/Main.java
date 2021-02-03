5a657a79ca9ffe2ad4275175a9fc735e80e4cd22
Akhila Muppavarapu <akhila.muppavarapu2@gmail.com>
2021-02-02 19:35:43 -0500

    tester code for circle added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Circle circle = new Circle(2.5);
+        System.out.println("Perimeter of Circle:  " + circle.getPerimeter());
+        System.out.println("Area of Circle:  " + circle.getArea());
+
     }
 }
