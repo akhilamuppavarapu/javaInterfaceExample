c482f90f0c45547937842fc25c1a37913535ff7e
Akhila Muppavarapu <akhila.muppavarapu2@gmail.com>
2021-02-02 19:38:03 -0500

    resizable circle implements its own resizable radius interface

---
+++ src/com/company/Resizable.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public interface Resizable {
+
+    double resize(int percentage);
+
+}
--- src/com/company/ResizableCircle.java
+++ src/com/company/ResizableCircle.java
@@ -1,8 +1,14 @@
 package com.company;
 
-public class ResizableCircle extends Circle implements GeometricObject{
+public class ResizableCircle extends Circle implements Resizable{
 
     public ResizableCircle(double radius) {
         super(radius);
+    }
+
+    @Override
+    public double resize(int percentage) {
+//        return 0;
+        return radius * percentage / 100;
     }
 }
