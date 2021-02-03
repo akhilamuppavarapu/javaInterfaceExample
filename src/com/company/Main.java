0b4bad757bf141f2889bfdae93c0c241c70eb35f
Akhila Muppavarapu <akhila.muppavarapu2@gmail.com>
2021-02-02 19:38:38 -0500

    tester methods for resizable circle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,8 @@ public class Main {
         System.out.println("Perimeter of Circle:  " + circle.getPerimeter());
         System.out.println("Area of Circle:  " + circle.getArea());
 
+        ResizableCircle resizableCircle = new ResizableCircle(5);
+        System.out.println("Radius of new circle resized by 50% :  " + resizableCircle.resize(50));
+
     }
 }
